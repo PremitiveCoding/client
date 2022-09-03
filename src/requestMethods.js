82108a1fe91ba95f6c32721399b005a47bac73fd
PremitiveCoding <58826270+PremitiveCoding@users.noreply.github.com>
2022-09-03 13:31:39 +0200

    ok

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://yeshopro.herokuapp.com/";
+const BASE_URL = "https://rifshopro.herokuapp.com/";
 
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
